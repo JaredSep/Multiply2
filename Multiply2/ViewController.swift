890f79adf17f9e749908456027fc84bc3fb3f7e5
Jared Sepulveda <Sepulvedajared20@gmail.com>
2023-02-24 10:01:19 -0700

    Initial Commit

---
+++ Multiply2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiply2
+//
+//  Created by Sepulveda Torres, Jared - Student on 2/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
